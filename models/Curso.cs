0472739287245238d1c27b47a24035ad4895194f
biel.barros <gabrielbarrosg11@gmail.com>
2025-07-22 15:12:29 -0300

    feat: Implement Curso class with properties for Nome and Alunos

---
+++ models/Curso.cs
@@ -0,0 +1,15 @@
+namespace Console_projects;
+using System;
+using System.Collections.Generic;
+using models.Pessoa;
+
+public class Curso
+{
+    public string Nome { get; set; }
+    public List<Pessoa> Alunos { get; set; }
+
+    public void AdicionarAluno(Pessoa aluno)
+    {
+        Alunos.Add(aluno);
+    }
+}
