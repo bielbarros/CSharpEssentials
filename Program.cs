e2568992709b6d7506d67f4b2b5a68089700b3a2
biel.barros <gabrielbarrosg11@gmail.com>
2025-07-25 13:53:53 -0300

    feat: implements example of business rules for user permissions

--- Program.cs
+++ Program.cs
@@ -428,9 +428,18 @@ int saleAmount = 1001;
 
 Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
 
+*/
 
+
+/*
+Randoms:
+
+Random coin = new Random();
+int flip = coin.Next(0, 2);
+Console.WriteLine((flip == 0) ? "heads" : "tails");
 
 */
+
 
 /*
 // Operadores aritméticos
@@ -1073,5 +1082,96 @@ string[] linhas = File.ReadAllLines("arquivos/leitura.txt"); // Lê todas as lin
 foreach (string linha in linhas)
 {
     Console.WriteLine(linha); // Imprime cada linha do arquivo
+}
+*/
+
+
+/*
+
+REGRAS DE NEGÓCIO:
+
+Se o usuário for um administrador com um nível maior que 55, exiba a mensagem:
+Welcome, Super Admin user.
+
+
+Se o usuário for um administrador com um nível menor ou igual a 55, exiba a mensagem:
+Welcome, Admin user.
+
+
+Se o usuário for um gerente com um nível maior ou igual a 20, exiba a mensagem:
+Contact an Admin for access.
+
+
+Se o usuário for um gerente com um nível menor que 20, exiba a mensagem:
+You do not have sufficient privileges.
+
+
+Se o usuário não for um Administrador ou um Gerente, gere a mensagem:
+You do not have sufficient privileges.
+
+*/
+
+/*
+string permission = "Admin|Manager";
+int level = 55;
+string adminLevel;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        adminLevel = "Super Admin";
+        Console.WriteLine($"Welcome, {adminLevel} user.");
+    }
+    else
+    {
+        adminLevel = "Admin";
+        Console.WriteLine($"Welcome, {adminLevel} user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+        return;
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+        return;
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+    return;
+}
+*/
+
+
+// REFATORAÇÃO:
+
+/*
+string permission = "Admin|Manager";
+int level = 55;
+
+CheckUserAccess(permission, level);
+
+void CheckUserAccess(string permission, int level)
+{
+    if (permission.Contains("Admin"))
+    {
+        Console.WriteLine(level > 55 ? "Welcome, Super Admin user." : "Welcome, Admin user.");
+        return;
+    }
+
+    if (permission.Contains("Manager"))
+    {
+        Console.WriteLine(level >= 20 ? "Contact an Admin for access." : "You do not have sufficient privileges.");
+        return;
+    }
+
+    Console.WriteLine("You do not have sufficient privileges.");
 }
 */
