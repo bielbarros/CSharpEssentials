7d50c09946456c4d8a65b3884d17f40b11622213
biel.barros <gabrielbarrosg11@gmail.com>
2025-07-29 14:04:46 -0300

    feat: Add data formatting challenge with solution

--- Program.cs
+++ Program.cs
@@ -2069,3 +2069,75 @@ Saída:
  Saída:
  769C  Mr. Stephen Ortega       $5,000.00
 */
+
+
+/*
+
+DESAFIO DE CÓDIGO:
+
+
+Use o novo conhecimento que você adquiriu sobre formatação de cadeias de caracteres para criar um aplicativo que possa mesclar e formatar 
+o conteúdo adequado considerando a saída de exemplo acima. Preste muita atenção no espaço em branco e represente com precisão esse
+formato exato usando o C#. Selecione e exclua todas as linhas de código no Editor do Visual Studio Code.
+No Visual Studio Code, adicione o seguinte código para obter os dados para o desafio: 
+
+string customerName = "Ms. Barros";
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+// Your logic here
+Console.WriteLine("Here's a quick comparison:\n");
+string comparisonMessage = "";
+
+// Your logic aqui
+comparisonMessage += currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+Console.WriteLine(comparisonMessage);
+Use o Editor do Visual Studio Code para gerar a mensagem com as variáveis e o código fornecidos. Você não pode excluir nenhum código existente, exceto os comentários. Verifique se o código gera a seguinte mensagem: Output Copiar Dear Ms. Barros, As a customer of our Magic Yield offering we are excited to tell you about a new financial product that would dramatically increase your return. Currently, you own 2,975,000.00 shares at a return of 12.75%. Our new product, Glorious Future offers a return of 13.13%. Given your current volume, your potential profit would be $63,000,000.00. Here's a quick comparison: Magic Yield 12.75% $55,000,000.00 Glorious Future 13.13% $63,000,000.00 Boa sorte!
+*/
+
+
+
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+Console.WriteLine(comparisonMessage);
