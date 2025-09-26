dbd10b464fb0de7e52ee9bc39f211af7a009ef90
biel.barros <gabrielbarrosg11@gmail.com>
2025-09-26 18:43:25 -0300

    fix: Ensure Conta class properly initializes account number in constructor

--- models/Conta.cs
+++ models/Conta.cs
@@ -5,6 +5,12 @@ namespace Console_projects;
 public abstract class Conta
 {
     protected decimal saldo;
+    private int numeroConta;
+
+    public Conta(int numeroConta)
+    {
+        this.numeroConta = numeroConta;
+    }
 
     public abstract void Creditar(decimal valor);
 
