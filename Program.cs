f90e095f65fd31935d20abea2ce87b48364c7357
biel.barros <gabrielbarrosg11@gmail.com>
2025-09-26 18:43:20 -0300

    fix: Update ContaCorrente and ContaPoupanca instantiation to include account number

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using Console_projects;
 
-ContaCorrente contaCorrente = new ContaCorrente(0.05m); // 5% interest rate
+ContaCorrente contaCorrente = new ContaCorrente(0.05m, 111); // 5% interest rate
 contaCorrente.Creditar(100); // Deposit 100
 contaCorrente.ObterSaldo(); // Should return 105 after interest
 
-ContaPoupanca contaPoupanca = new ContaPoupanca();
+ContaPoupanca contaPoupanca = new ContaPoupanca(12345); // Provide a valid account number
 contaPoupanca.Creditar(200); // Deposit 200
 contaPoupanca.CalcularRendimento(); // Apply interest
